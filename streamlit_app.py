d8bf8fb9c738adde66b1cfb921c69bc4b321e1e1
Sonjemin <jemin68003726@gmail.com>
2025-04-30 15:19:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,17 @@
 import streamlit as st
 st.text_input(label, type="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")
+
+from openai import OpenAI
+client = OpenAI()
+response = client.responses.create(
+ model="gpt-4o",
+ input=[
+ {
+ "role": "user",
+ "content": "Write a onesentence
+bedtime story about a 
+unicorn."
+ }
+ ]
+)
+print(response.output_text)
